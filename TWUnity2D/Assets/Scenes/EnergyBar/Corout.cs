8ed22acc35ae939e31c359cca1b2e2a5cdd18d0e
PAULO RUBENS WERNECK CASTOR <paulorwc1@gmail.com>
2016-02-28 15:16:06 -0300

    Iniciando estudos de Energy Bar Caso seja necessário voltar o repositório, usar aqui

---
+++ TWUnity2D/Assets/Scenes/EnergyBar/Corout.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Corout : MonoBehaviour {
+
+	public float forca = 2.0F;
+	public Rigidbody2D bola;
+	public bool interruptor = true;
+
+	void Start () {
+
+		StartCoroutine (move());
+	}
+	
+
+
+	public IEnumerator move()
+	{
+		while(interruptor == true)
+		{
+			yield return new WaitForSeconds(2);
+			bola.AddForce (new Vector2 (0.0F, forca),ForceMode2D.Impulse);
+		}
+	}
+}
